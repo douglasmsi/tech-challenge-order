8413fd8d53876f7d80873694a7f2706eeefac020
douglasmsi <douglas.marra@mercadolivre.com>
2024-01-04 21:41:57 -0300

    refactor

--- src/main/java/br/com/fiap/postech/fastfood/TechChallengeOrderApplication.java
+++ src/main/java/br/com/fiap/postech/fastfood/TechChallengeOrderApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class FastfoodApplication {
+public class TechChallengeOrderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FastfoodApplication.class, args);
+		SpringApplication.run(TechChallengeOrderApplication.class, args);
 	}
 
 }
--- src/main/java/br/com/fiap/postech/fastfood/infrastructure/clients/TechChallengePayment.java
+++ src/main/java/br/com/fiap/postech/fastfood/infrastructure/clients/TechChallengePayment.java
@@ -3,7 +3,7 @@ package br.com.fiap.postech.fastfood.infrastructure.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-//@FeignClient(name = "tech-challenge-client", url = "${spring.cloud.openfeign.client.config.tech-challenge-payment-alb}")
+@FeignClient(name = "tech-challenge-payment")
 public interface TechChallengePayment {
 
 
