7e5e7218505eec7c34977a0b8b3a4561d76e375d
douglasmsi <douglas.marra@mercadolivre.com>
2024-01-07 12:39:04 -0300

    refactor

--- src/main/java/br/com/fiap/postech/fastfood/controller/dto/CriarPedidoRequest.java
+++ src/main/java/br/com/fiap/postech/fastfood/controller/dto/CriarPedidoRequest.java
@@ -2,11 +2,13 @@ package br.com.fiap.postech.fastfood.controller.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 public class CriarPedidoRequest {
 
   private String clienteCpf;
